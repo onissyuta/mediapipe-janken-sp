f82a35cb4df2145064fee6326e89822faea31ae1
onissyuta <dev@pixelog.net>
2023-10-03 18:26:01 +0900

    [clean] Handsの読み込みタイミングを変更

--- src/script.js
+++ src/script.js
@@ -34,26 +34,6 @@ audioElements.forEach(elm => {
 });
 
 
-
-// Mediapipeの初期化
-const vision = await FilesetResolver.forVisionTasks(
-    // path/to/wasm/root
-    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
-);
-
-const handLandmarker = await HandLandmarker.createFromOptions(
-    vision,
-    {
-        baseOptions: {
-            modelAssetPath: "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task",
-            delegate: "GPU",
-        },
-        runningMode: "VIDEO",
-        numHands: 1
-    });
-
-
-
 dialog.showModal();
 
 
@@ -106,6 +86,24 @@ document.getElementById('startBtn').addEventListener('click', async () => {
             }
         )
 });
+
+
+// Mediapipeの初期化
+const vision = await FilesetResolver.forVisionTasks(
+    // path/to/wasm/root
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+);
+
+const handLandmarker = await HandLandmarker.createFromOptions(
+    vision,
+    {
+        baseOptions: {
+            modelAssetPath: "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task",
+            delegate: "GPU",
+        },
+        runningMode: "VIDEO",
+        numHands: 1
+    });
 
 
 
